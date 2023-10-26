7709c34c272da3708951d9516b9a2a0f3333d466
mrmeepmeep <myachou42@gmail.com>
2023-10-26 16:12:37 +0200

    -

--- script.js
+++ script.js
@@ -1,33 +1,69 @@
 
 
-const container = document.querySelector('.container')
+const container = document.querySelector('.grid-container')
 const blackBtn = document.querySelector('#blackBtn')
-const greenBtn = document.querySelector('#greenBtn')
-const redBtn = document.querySelector('#redBtn')
+const randomBtn = document.querySelector('#randomBtn')
+const grayBtn = document.querySelector('#grayBtn')
 const eraserBtn = document.querySelector('#eraser')
+const resetBtn = document.querySelector("#reset")
 
-function createDivs(numOfDivs) {
-    for (let i = 0; i < numOfDivs; i++){
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('newDiv')
-        newDiv.style.backgroundColor = 'white'
-        container.appendChild(newDiv)
+
+
+function createDiv(num) {
+    container.style.display = 'grid'
+    container.style.gridTemplateRows = `repeat(${num} ,1fr)`;
+    container.style.gridTemplateColumns = `repeat(${num} ,1fr)`;
+    for(let i = 0; i < num* num; i++) {
+        let cell = document.createElement('div');
+        cell.setAttribute('id', 'cell')
+        container.appendChild(cell)
+
+        blackBtn.addEventListener('click' ,() => {
+            cell.addEventListener('mouseover', function (event) {
+            event.target.style.backgroundColor = 'black'; 
+            event.target.style.border='none'
+            })
+        })
+
+        grayBtn.addEventListener('click' ,() => {
+            cell.addEventListener('mouseover', function (event) {
+            event.target.style.backgroundColor = `rgba(0, 0, 0, 0.1)`; 
+            event.target.style.border='none'
+            })
+        })
+
+        randomBtn.addEventListener('click' ,() => {
+            cell.addEventListener('mouseover', function (event) {
+            event.target.style.backgroundColor = randomColor(); 
+            event.target.style.border='none'
+            })
+        })
+
+        eraserBtn.addEventListener('click' ,() => {
+            cell.addEventListener('mouseover', function (event) {
+            event.target.style.backgroundColor = 'whitesmoke'; 
+            event.target.style.border='1px solid rgba(0, 0, 0, 0.030)';
+            })
+        })
+
+        resetBtn.addEventListener('click', () => {
+            cell.style.backgroundColor='whitesmoke'
+            cell.style.border = '1px solid rgba(0, 0, 0, 0.030)';
+        })
+
     }
 }
-createDivs(16*16)
 
-
-/* let numOfDivs;
-
-
-function createDivs(num) {
-    for (let i = 0; i < num; i++){
-         numOfDivs[i] = document.createElement('div')
-         container.appendChild(numOfDivs)
-
-    }
-
+function randomColor(){
+    let a = Math.floor(Math.random() * 255) 
+    let b = Math.floor(Math.random() * 255) 
+    let c = Math.floor(Math.random() * 255) 
+    return `rgba(${a},${b},${c})`
 }
 
-createDivs(4)
-console.log(i) */
+createDiv(32)
+
+
+
+
+
