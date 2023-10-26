adf81535ed3815a422a010d73b758e00a329f151
mrmeepmeep <myachou42@gmail.com>
2023-10-26 15:59:52 +0200

    Fixed button functions & changed UI

--- script.js
+++ script.js
@@ -2,11 +2,10 @@
 
 const container = document.querySelector('.grid-container')
 const blackBtn = document.querySelector('#blackBtn')
-const greenBtn = document.querySelector('#greenBtn')
-const redBtn = document.querySelector('#redBtn')
+const randomBtn = document.querySelector('#randomBtn')
+const grayBtn = document.querySelector('#grayBtn')
 const eraserBtn = document.querySelector('#eraser')
 const resetBtn = document.querySelector("#reset")
-
 
 
 
@@ -19,16 +18,50 @@ function createDiv(num) {
         cell.setAttribute('id', 'cell')
         container.appendChild(cell)
 
-        
+        blackBtn.addEventListener('click' ,() => {
             cell.addEventListener('mouseover', function (event) {
-            event.target.style.backgroundColor = 'blue'; 
+            event.target.style.backgroundColor = 'black'; 
             event.target.style.border='none'
             })
-   
+        })
+
+        grayBtn.addEventListener('click' ,() => {
+            cell.addEventListener('mouseover', function (event) {
+            event.target.style.backgroundColor = `rgba(0, 0, 0, 0.1)`; 
+            event.target.style.border='none'
+            })
+        })
+
+        randomBtn.addEventListener('click' ,() => {
+            cell.addEventListener('mouseover', function (event) {
+            event.target.style.backgroundColor = randomColor(); 
+            event.target.style.border='none'
+            })
+        })
+
+        eraserBtn.addEventListener('click' ,() => {
+            cell.addEventListener('mouseover', function (event) {
+            event.target.style.backgroundColor = 'whitesmoke'; 
+            event.target.style.border='1px solid rgba(0, 0, 0, 0.030)';
+            })
+        })
+
+        resetBtn.addEventListener('click', () => {
+            cell.style.backgroundColor='whitesmoke'
+            cell.style.border = '1px solid rgba(0, 0, 0, 0.030)';
+        })
+
     }
 }
-createDiv(32)
 
+function randomColor(){
+    let a = Math.floor(Math.random() * 255) 
+    let b = Math.floor(Math.random() * 255) 
+    let c = Math.floor(Math.random() * 255) 
+    return `rgba(${a},${b},${c})`
+}
+
+createDiv(32)
 
 
 
